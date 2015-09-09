9ee65fe9c5863d5c7f11edf89a7fe2115ae02ead
hung <pdhung3012@gmail.com>
2015-09-09 08:01:01 -0500

    change

--- ass1/PA1/test.sh
+++ ass1/PA1/test.sh
@@ -1,6 +1,6 @@
 ../../bin/coolc stack.cl atoi.cl;
 
-for i in {1..2}
+for i in {1..10}
 do
   echo 'Executing test' $i
   ../../bin/spim -file stack.s < ./test/testcases/test$i.txt | head -n -1 | tail -n +6 > ./test/ourlastresults/result$i.txt;
