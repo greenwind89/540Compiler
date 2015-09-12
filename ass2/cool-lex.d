6cacea3468f3b9f6a6837ef5bb7077ad993b036b
Minh Anh Truong - student <minh@pyrite-n1.cs.iastate.edu>
2015-09-11 22:52:56 -0500

    add assignment 2

---
+++ ass2/cool-lex.d
@@ -0,0 +1,2 @@
+cool-lex.o cool-lex.d : cool-lex.cc cool-parse.h copyright.h cool-io.h tree.h \
+ stringtab.h list.h stringtab.h utilities.h
---
+++ ass2/lextest.d
@@ -0,0 +1,2 @@
+lextest.o lextest.d : lextest.cc copyright.h cool-parse.h cool-io.h tree.h \
+ stringtab.h list.h utilities.h
