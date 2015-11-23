c5bc7f3d2ba1fbb16c07c828c2f5bc08f74c352d
Minh Anh Truong - student <minh@pyrite-n2.cs.iastate.edu>
2015-11-23 14:33:46 -0600

    init ass5 with test

---
+++ ass5/test.sh
@@ -0,0 +1,14 @@
+make cgen
+for i in {1..1}
+do
+  echo 'Executing test' $i
+  ./mycoolc ./test/testcases/test$i.cl -o ./test/ourtemp/test$i.s;
+
+  ../bin/coolc ./test/testcases/test$i.cl  -o ./test/theirtemp/test$i.s;
+
+  ../bin/spim ./test/ourtemp/test$i.s > ./test/ourresults/result$i.txt 2>&1;
+
+  ../bin/spim ./test/theirtemp/test$i.s > ./test/theirresults/result$i.txt 2>&1;
+
+  diff ./test/ourresults/result$i.txt ./test/theirresults/result$i.txt
+done
---
+++ ass5/test/testcases/test1.cl
@@ -0,0 +1,5 @@
+Class Main inherits IO{
+  main(): Object {
+    out_string("Hello")
+  };
+};
